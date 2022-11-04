e71fcc1a9ff0b4fff57943700cfe65653a45bb5f
DanySK <danilo.pianini@gmail.com>
2022-11-04 10:25:37 +0000

    Deploying to solutions from @ APICe-at-DISI/exercises@c92a316ff171e45573da3135a0681a817da1260c 🚀

--- 61-simple-gui/build.gradle.kts
+++ 61-simple-gui/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     application
     java
+    id("org.danilopianini.gradle-java-qa") version "0.39.0"
 }
 
 repositories {
--- 61-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
+++ 61-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
@@ -1,8 +1,5 @@
 package it.unibo.mvc;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
@@ -10,8 +7,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 /**
  * This class is a simple application that writes a random number on a file.
+ * 
  * This application does not exploit the model-view-controller pattern, and as
  * such is just to be used to learn the basics, not as a template for your
  * applications.
@@ -19,6 +23,7 @@ import java.util.Random;
 public class MiniGUI {
 
     private static final String TITLE = "A very simple GUI application";
+    private static final String RESULT_TXT_CONTENT = "Result";
     private static final int PROPORTION = 5;
     private final Random randomGenerator = new Random();
     private final JFrame frame = new JFrame(TITLE);
@@ -29,8 +34,19 @@ public class MiniGUI {
     public MiniGUI() {
         final JPanel canvas = new JPanel();
         canvas.setLayout(new BorderLayout());
+        /*
+         * Part 1
+         */
+        final JPanel aPanel = new JPanel();
+        aPanel.setLayout(new BoxLayout(aPanel, BoxLayout.LINE_AXIS));
+        canvas.add(aPanel, BorderLayout.CENTER);
         final JButton write = new JButton("Print a random number on standard output");
-        canvas.add(write, BorderLayout.CENTER);
+        aPanel.add(write);
+        /*
+         * Part 2
+         */
+        final JTextField result = new JTextField(RESULT_TXT_CONTENT);
+        canvas.add(result, BorderLayout.NORTH);
         frame.setContentPane(canvas);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         /*
@@ -39,7 +55,12 @@ public class MiniGUI {
         write.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(final ActionEvent e) {
-                System.out.println(randomGenerator.nextInt());
+                /*
+                 * Part 3
+                 */
+                final int res = randomGenerator.nextInt();
+                System.out.println(res); // NOPMD
+                result.setText(RESULT_TXT_CONTENT + ": " + Integer.toString(res));
             }
         });
     }
