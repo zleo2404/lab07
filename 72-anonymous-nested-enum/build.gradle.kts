7637cc3d52020ec9583086a969c3bf22fe31d071
DanySK <danilo.pianini@gmail.com>
2024-07-09 10:29:46 +0000

    Deploying to exercises from @ APICe-at-DISI/exercises@c939e32609215b7d9a0eaf847a0c8357b669aa77 🚀

--- 72-anonymous-nested-enum/build.gradle.kts
+++ 72-anonymous-nested-enum/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("org.danilopianini.gradle-java-qa") version "1.53.0"
+    id("org.danilopianini.gradle-java-qa") version "1.55.0"
 }
 
 repositories {
@@ -8,9 +8,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:5.10.3")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.3")
 }
 
 tasks.test {
