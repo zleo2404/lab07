ac571c993d9eb8885f78a107b0e954cad447aa1a
Leonardo <st10151@iismarconipieralisi.it>
2024-11-07 18:02:23 +0100

    Ex 72 part 2 completed

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -4,18 +4,100 @@ import java.util.Comparator;
 import java.util.Locale;
 import java.util.Objects;
 
+import javax.swing.JPopupMenu.Separator;
+
 /**
  * Implementation of {@link MonthSorter}.
  */
 public final class MonthSorterNested implements MonthSorter {
+    public enum Month{
+        JANUARY("JANUARY"),
+        FEBRUARY("FEBRUARY"),
+        MARCH("MARCH"),
+        APRIL("APRIL"),
+        MAY("MAY"),
+        JUNE("JUNE"),
+        JULY("JULY"),
+        AUGUST("AUGUST"),
+        SEPTEMBER("SEPTEMBER"),
+        OCTOBER("OCTOBER"),
+        NOVEMBER("NOVEMBER"),
+        DECEMBER("DECEMBER");
+        
+        private final String actualName;
 
-    @Override
-    public Comparator<String> sortByDays() {
-        return null;
+        Month(String string) {
+            this.actualName= string;
+        }
+
+        public static Month MonthFromString(String string){
+                    int i=0;
+                    Month value = null;
+                    for(Month s : Month.values())
+                    {
+                        if(s.actualName.contains(string.toUpperCase())){
+                            i++;
+                            value = s;
+                        }
+                    }
+                    if(i>1) throw new IllegalArgumentException("Nome ambiguo");
+                    if(i==0) throw new IllegalArgumentException("Non esiste");
+        
+                    return value;
+                    
+                }
+        
+                public static int getDays(Month m){
+        
+                    if(m.actualName.equals(NOVEMBER.name()) || m.actualName.equals(APRIL.name()) || m.actualName.equals(JUNE.name()) || m.actualName.equals(SEPTEMBER.name())) return 30;
+                    if(m.actualName.equals(FEBRUARY.name())) return 28;
+        
+                    return 31;
+        
+                }
+        
+            }
+        
+        
+        
+        
+            @Override
+            public Comparator<String> sortByDays() {
+                return new Comparator<String>() {
+        
+                    @Override
+                    public int compare(String o1, String o2) {
+                       
+                            if(Month.getDays(Month.MonthFromString(o1)) > Month.getDays(Month.MonthFromString(o2))){
+                                return 1;
+                            }else if ( Month.getDays(Month.MonthFromString(o1)) == Month.getDays(Month.MonthFromString(o2))) return 0;
+                            return -1;
+
+                                                
+            }
+            
+        };
     }
 
     @Override
     public Comparator<String> sortByOrder() {
-        return null;
+        return new Comparator<String>() {
+
+            @Override
+            public int compare(String o1, String o2) {
+
+                
+                    if(Month.MonthFromString(o1).ordinal()>Month.MonthFromString(o2).ordinal()){
+                        return 1;
+                    }else if(Month.MonthFromString(o1).ordinal()==Month.MonthFromString(o2).ordinal()){ return 0;
+                    }else{
+                        return -1;
+                    }
+                
+                
+                
+            }
+            
+        };
     }
 }
